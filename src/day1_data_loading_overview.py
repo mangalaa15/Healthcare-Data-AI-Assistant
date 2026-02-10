63943fa3ab5c463a4177ff51a2d02f64e886d90b
mangalaa15 <93286361+mangalaa15@users.noreply.github.com>
2026-02-09 23:14:14 -0800

    Update day1_data_loading_overview.py

--- src/day1_data_loading_overview.py
+++ src/day1_data_loading_overview.py
@@ -1,1 +1,10 @@
-
+import pandas as pd
+#load the dataset
+diabetes_data = pd.read_csv("../data/diabetes_data.csv")
+#Quick Overview
+print("first 5 rows:")
+primt(diabetes_data.head())
+print(\nDataset info:")
+print("diabetes_data.info()")
+print("\nStatistical summary:")
+print(diabetes_data.describe())
